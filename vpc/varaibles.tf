3b7b26babc1dd3990c86d2e29ec27f97c4eb8ff9
fpignataro <>
2020-12-22 13:07:51 -0300

    tags en todo

--- vpc/varaibles.tf
+++ vpc/varaibles.tf
@@ -1,27 +1,8 @@
-#Module      : LABEL
-#Description : Terraform label module variables.
+
 variable "name" {
   type        = string
   default     = ""
   description = "Name  (e.g. `app` or `cluster`)."
-}
-
-variable "application" {
-  type        = string
-  default     = ""
-  description = "Application (e.g. `cd` or `clouddrove`)."
-}
-
-variable "environment" {
-  type        = string
-  default     = ""
-  description = "Environment (e.g. `prod`, `dev`, `staging`)."
-}
-
-variable "label_order" {
-  type        = list
-  default     = []
-  description = "Label order, e.g. `name`,`application`."
 }
 
 variable "delimiter" {
