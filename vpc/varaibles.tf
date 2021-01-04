bb5640651df3a443b35c5f50e359b282ef2b1ba9
fpignataro <>
2021-01-04 14:13:38 -0300

    ip range manual

--- vpc/varaibles.tf
+++ vpc/varaibles.tf
@@ -28,11 +28,6 @@ variable "description" {
   description = "A free-form text field up to a limit of 255 characters to describe the VPC."
 }
 
-variable "ip_range" {
-  type        = string
-  description = "The range of IP addresses for the VPC in CIDR notation. Network ranges cannot overlap with other networks in the same account and must be in range of private addresses as defined in RFC1918. It may not be larger than /16 or smaller than /24."
-}
-
 variable "tenant" {
   type        = string
   default     = ""
